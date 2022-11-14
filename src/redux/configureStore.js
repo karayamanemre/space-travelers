b60f86ae8ac02c1ba03bd9d1def1ecdc8af70ebc
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-14 21:12:04 +0300

    linters fix

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import missionsReducer from './missions/missions';
 
-
 const rootReducer = combineReducers({
   missions: missionsReducer,
 });
