e635ff216000dc451be25075eb1f7a92c724facd
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-14 21:53:32 +0300

    linters fix

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -5,12 +5,11 @@ export default function NavLinks() {
     textDecoration: 'underline',
   };
   return (
-  <>
-    <NavLink to="/" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Rockets</NavLink>
-    <NavLink to="/Missions" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Missions</NavLink>
-    <p>|</p>
-    <NavLink to="/Profile" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Profile</NavLink>
-  </>
+    <>
+      <NavLink to="/" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Rockets</NavLink>
+      <NavLink to="/Missions" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Missions</NavLink>
+      <p>|</p>
+      <NavLink to="/Profile" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Profile</NavLink>
+    </>
   );
 }
-
