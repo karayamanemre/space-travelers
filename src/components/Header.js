2f448d8e6ae5a821b461029d169465c8c6016327
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-14 13:41:01 +0300

    fix linters errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,37 +5,37 @@ import './styles/Header.css';
 
 export default function header() {
   const activeStyle = {
-    textDecoration: 'underline'
+    textDecoration: 'underline',
   };
 
   return (
     <header>
       <div className="logo">
-          <img src={logo} className="logo-img" alt="logo" />
-          <h1>
-            Space Traveler`s Hub
-          </h1>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/Rockets" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-                Rockets
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Missions" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-                Missions
-              </NavLink>
-            </li>
-            <li>|</li>
-            <li>
-              <NavLink to="/Profile" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-                My Profile
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        <img src={logo} className="logo-img" alt="logo" />
+        <h1>
+          Space Traveler`s Hub
+        </h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/Rockets" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Missions" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+              Missions
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink to="/Profile" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
 
   );
