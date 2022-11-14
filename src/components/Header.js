07604fdf3a722aa9b8ea2b179fbf7c660afed082
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-14 13:36:52 +0300

    create components

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/planet.png';
+import './styles/Header.css';
+
+export default function header() {
+  const activeStyle = {
+    textDecoration: 'underline'
+  };
+
+  return (
+    <header>
+      <div className="logo">
+          <img src={logo} className="logo-img" alt="logo" />
+          <h1>
+            Space Traveler`s Hub
+          </h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/Rockets" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+                Rockets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Missions" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+                Missions
+              </NavLink>
+            </li>
+            <li>|</li>
+            <li>
+              <NavLink to="/Profile" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+                My Profile
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+    </header>
+
+  );
+}
