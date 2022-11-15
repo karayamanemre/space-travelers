9505de01e5df51c24549331985be161af2f381bf
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-15 17:21:52 +0300

    missions add to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './styles/Profile.css';
 
-export default function Profile() {
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+  const activeMissions = missions.filter((mission) => mission.reserved);
+
   return (
-    <div>
-      <h1>Test profile</h1>
-    </div>
+    <section>
+      <div className="profile-missions">
+        <h2>My Missons</h2>
+        <table className="profile-mission-title">
+          <tbody>
+            {activeMissions.map((mission) => (
+              <tr key={mission.id} className="profile-mission-title">
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="rocket-list col-5">
+        <h2>My Rockets</h2>
+      </div>
+    </section>
   );
-}
+};
+
+export default Profile;
