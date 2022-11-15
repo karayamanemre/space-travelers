d4673ad2681c5d19d62c1a648c296f94ca4fab85
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-15 17:29:33 +0300

    style changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import './styles/Profile.css';
+import styles from './styles/Profile.module.css';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missions);
   const activeMissions = missions.filter((mission) => mission.reserved);
 
   return (
-    <section>
-      <div className="profile-missions">
+    <section className={styles.profile_section}>
+      <div className={styles.profile_missions}>
         <h2>My Missons</h2>
-        <table className="profile-mission-title">
+        <table className={styles.profile_mission_title}>
           <tbody>
             {activeMissions.map((mission) => (
-              <tr key={mission.id} className="profile-mission-title">
+              <tr className={styles.profile_mission_tr} key={mission.id}>
                 <td>{mission.mission_name}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <div className="rocket-list col-5">
+      <div>
         <h2>My Rockets</h2>
       </div>
     </section>
