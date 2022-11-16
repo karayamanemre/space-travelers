27d5355a574bbe136e01d2dc9a8dd4e5413332fd
vitalis <vitalismutwiri2gmail.com>
2022-11-16 11:56:48 +0300

    Add table to profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,8 @@ import styles from './styles/Profile.module.css';
 const Profile = () => {
   const missions = useSelector((state) => state.missions);
   const activeMissions = missions.filter((mission) => mission.reserved);
+  const rockets = useSelector((state) => state.rockets);
+  const activeRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <section className={styles.profile_section}>
       <div className={styles.profile_missions}>
@@ -19,13 +21,15 @@ const Profile = () => {
           </tbody>
         </table>
       </div>
-      <div>
+      <div className={styles.profile_missions}>
         <h2>My Rockets</h2>
-        <table>
+        <table className={styles.profile_mission_title}>
           <tbody>
-            <tr>
-              <td />
-            </tr>
+            {activeRockets.map((rocket) => (
+              <tr className={styles.profile_mission_tr} key={rocket.id}>
+                <td className={styles.profile_mission_td}>{rocket.name}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
